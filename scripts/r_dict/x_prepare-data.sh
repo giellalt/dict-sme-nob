56edd6331b99238a886928c262480ff848abfe69
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-11-04 10:04:42 +0000

    extened helping script

--- scripts/r_dict/x_prepare-data.sh
+++ scripts/r_dict/x_prepare-data.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
+
+# doubling tests
+# rm -f test-results.xml
+# java -Xmx2048m net.sf.saxon.Transform -it main check_entry.xsl inDir=init_dir
+
+echo	"cleaning up ... "
 rm -rf init_dir
+
 #dicts/smenob/scripts/r_dict
+
+echo	"removing meta info like license and the like"
 java -Xmx2048m net.sf.saxon.Transform -it main filter_meta-info.xsl inDir=../../src
 
 echo	"removing xhtml namespace"
-
 perl -pi -e 's/^(<r) xmlns.*(>)$/$1$2/' init_dir/*.xml 
+
+# revert dict dir
+#java net.sf.saxon.Transform -it main revert_dict_dir.xsl inDir=init_dir
+
+echo	"adding pos phrase to all tf"
+# add pos="phrase"
+java net.sf.saxon.Transform -it main add_phrase_pos.xsl inDir=init_dir
+
+echo	"adding meta info like license and the like"
+java -Xmx2048m net.sf.saxon.Transform -it main add_meta-info.xsl inDir=outx
+
+echo    " ....... removing xhtml namespace"
+
+perl -pi -e 's/^(<r) xmlns:xhtml=\".*xhtml\"(.*)$/$1$2/' outxxx/*.xml 
+
+echo	"Done!"
+
 
