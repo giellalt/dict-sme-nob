9f1d369b4515d36f4a2330256e6406d075341a2b
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-11-23 21:19:52 +0000

    added a working directory for the sorting task

--- scripts/r_dict/doIt.sh
+++ scripts/r_dict/doIt.sh
@@ -5,30 +5,31 @@
 # rm -f test-results.xml
 # java -Xmx2048m net.sf.saxon.Transform -it main check_entry.xsl inDir=init_dir
 
-echo	"cleaning up ... "
+echo	"cleaning up: removing init_dir ... "
 rm -rf init_dir
 
 #dicts/smenob/scripts/r_dict
 
-echo	"removing meta info like license and the like"
+echo	"removing meta info: license and the like"
 java -Xmx2048m net.sf.saxon.Transform -it main filter_meta-info.xsl inDir=../../src
 
 echo	"removing xhtml namespace"
 perl -pi -e 's/^(<r) xmlns.*(>)$/$1$2/' init_dir/*.xml 
 
 # revert dict dir
-#java net.sf.saxon.Transform -it main revert_dict_dir.xsl inDir=init_dir
+java net.sf.saxon.Transform -it main revert_dict_dir.xsl inDir=init_dir
 
-echo	"adding pos phrase to all tf"
+#echo	"adding pos phrase to all tf"
 # add pos="phrase"
-java net.sf.saxon.Transform -it main add_phrase_pos_smenob.xsl inDir=init_dir
+#java net.sf.saxon.Transform -it main add_phrase_pos_smenob.xsl inDir=init_dir
 
-echo	"adding meta info like license and the like"
-java -Xmx2048m net.sf.saxon.Transform -it main add_meta-info_smenob.xsl inDir=outx
+#echo	"adding meta info like license and the like"
+#java -Xmx2048m net.sf.saxon.Transform -it main add_meta-info_smenob.xsl inDir=outx
 
-echo    " ....... removing xhtml namespace"
-
-perl -pi -e 's/^(<r) xmlns:xhtml=\".*xhtml\"(.*)$/$1$2/' outxxx/*.xml 
+# check that properly!
+#   <xhtml:script src="../../scripts/gt_dictionary.js" type="text/javascript" xmlns:xhtml="http://www.w3.org/1999/xhtml" />
+#echo    " ....... removing xhtml namespace"
+#perl -pi -e 's/^(<r) xmlns:xhtml=\".*xhtml\"(.*)$/$1$2/' outxxx/*.xml 
 
 echo	"Done!"
 
