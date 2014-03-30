3f2653b0527d4c66d9452e7a1c87911287f403a3
Trond Trosterud <trond.trosterud@uit.no>
2014-03-30 15:04:31 +0000

    Corrected script.

--- smenob.sh
+++ smenob.sh
@@ -20,7 +20,7 @@ echo "LEXICON Root" > bin/smenob.lexc
 
 cat  src/*_smenob.xml | \
 egrep -v '<(lc|lsub|analysis)>' | \
-egrep -v '<lemma_ref' | \
+egrep -v '_re(fg)' | \
 tr '\n' '™' | sed 's/<e/£/g;'| tr '£' '\n'| \
 sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f6,16| \
 tr ' ' '_'| sed 's/:/%/g;'|tr '>' ':'| \
